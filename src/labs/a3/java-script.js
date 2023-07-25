cc355bd9f2a5a3219cb51c80410cbf0437acbd42
Nitin <ntnbhat9@gmail.com>
2023-07-24 21:03:57 -0400

    added a3 artifacts

---
+++ src/labs/a3/java-script.js
@@ -0,0 +1,41 @@
+import VariablesAndConstants from "./variables-and-constants";
+import VariableTypes from "./variable-types";
+import BooleanVariables from "./boolean-variables";
+import IfElse from "./if-else";
+import TernaryOperator from "./ternary-operator";
+import Es5Functions from "./es5-functions";
+import ArrowFunctions from "./arrow-functions";
+import ImpliedReturn from "./implied-return";
+import FunctionParanthesisAndParameters from "./function-paranthesis-and-parameters";
+import WorkingWithArrays from "./working-with-arrays";
+import TemplateLiterals from "./TemplateLiterals";
+import House from "./House";
+import Spread from "./spread";
+import Destructing from "./destructing";
+import FunctionDestructing from "./function-destructing";
+
+function JavaScript() {
+    console.log("Hello World!")
+    return (
+        <div>
+            <h1>JavaScript</h1>
+            <VariablesAndConstants/>
+            <VariableTypes/>
+            <BooleanVariables/>
+            <IfElse/>
+            <TernaryOperator/>
+            <Es5Functions/>
+            <ArrowFunctions/>
+            <ImpliedReturn/>
+            <FunctionParanthesisAndParameters/>
+            <WorkingWithArrays/>
+            <TemplateLiterals/>
+            <House/>
+            <Spread/>
+            <Destructing/>
+            <FunctionDestructing/>
+        </div>
+    );
+}
+
+export default JavaScript
